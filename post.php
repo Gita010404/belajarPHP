7cc315518692c942bd236983135f28f8005533c1
Gita010404 <1202305003@students.itspku.ac.id>
2025-10-07 11:22:28 +0700

    init

---
+++ post.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = $_POST['nama'];
+    $nim = $_POST['nim'];
+    $tanggal_lahir = $_POST['tanggal_lahir'];
+
+    echo "Nama saya : $nama <br>";
+    echo "NIM : $nim <br>";
+    echo "Tanggal Lahir : $tanggal_lahir <br>";
+?>
