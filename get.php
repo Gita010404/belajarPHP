6d3675b8685c6159d1421a053f4ff41bdbf7fa1d
afifah626 <1202305052@students.itspku.ac.id>
2025-10-07 10:55:06 +0700

    init

---
+++ get.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = $_GET['nama'];
+    $nim = $_GET['nim'];
+    $tanggal_lahir = $_GET['tanggal_lahir'];
+
+    echo "Nama saya : $nama <br>";
+    echo "NIM : $nim <br>";
+    echo "Tanggal Lahir : $tanggal_lahir <br>";
+?>
---
+++ latihan1.php
@@ -0,0 +1,11 @@
+<?php
+    $nim = '1202305003';
+    $nama = 'GITA CAHYANI';
+    $alamat = 'JAMBI';
+    $nilai = '100';
+
+    echo "NIM : $nim <br>";
+    echo "Nama : $nama <br>";
+    echo "Alamat : $alamat <br>";
+    echo "Nilai : $nilai <br>";
+?>
